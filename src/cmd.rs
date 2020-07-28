0c4e45add465f71366094b3377b370c6ded2367b
Johan Kristell <johan@jott.se>
2020-07-28 08:06:40 +0200

    Constants don't have to be public

--- src/cmd.rs
+++ src/cmd.rs
@@ -8,60 +8,66 @@ pub struct Cmd {
 /// Command Response type
 #[derive(Eq, PartialEq, Copy, Clone)]
 pub enum Response {
+    /// No response expected
     None = 0,
+    /// Short response expected
     Short = 1,
+    /// Long response expected
     Long = 3,
 }
 
-/// Go to idle
-pub const IDLE: (u8, Response)                  = (0, Response::None);
+/// Put card in idle mode
+const IDLE: (u8, Response)                  = (0, Response::None);
 /// Ask all cards to send their Cids
-pub const ALL_SEND_CID: (u8, Response)          = (2, Response::Long);
+const ALL_SEND_CID: (u8, Response)          = (2, Response::Long);
 /// Send Rca
-pub const SEND_REL_ADDR: (u8, Response)         = (3, Response::Short);
+const SEND_REL_ADDR: (u8, Response)         = (3, Response::Short);
 /// Select or deselect card
-pub const SEL_DESEL_CARD: (u8, Response)        = (7, Response::Short);
+const SEL_DESEL_CARD: (u8, Response)        = (7, Response::Short);
 ///
-pub const HS_SEND_EXT: (u8, Response)           = (8, Response::Short);
+const HS_SEND_EXT: (u8, Response)           = (8, Response::Short);
 ///
-pub const HS_SEND_CSD: (u8, Response)           = (9, Response::Long);
+const HS_SEND_CSD: (u8, Response)           = (9, Response::Long);
 ///
-pub const CMD13: (u8, Response)                 = (13, Response::Short);
+const CMD13: (u8, Response)                 = (13, Response::Short);
 ///
-pub const SET_BLOCKLEN: (u8, Response)          = (16, Response::Short);
+const SET_BLOCKLEN: (u8, Response)          = (16, Response::Short);
 ///
-pub const READ_SINGLE_BLOCK: (u8, Response)     = (17, Response::Short);
+const READ_SINGLE_BLOCK: (u8, Response)     = (17, Response::Short);
 ///
-pub const WRITE_SINGLE_BLOCK: (u8, Response)    = (24, Response::Short);
+const WRITE_SINGLE_BLOCK: (u8, Response)    = (24, Response::Short);
 ///
-pub const APP_OP_COMMAND: (u8, Response)        = (41, Response::Short);
+const APP_OP_COMMAND: (u8, Response)        = (41, Response::Short);
 ///
-pub const CMD51: (u8, Response)                 = (51, Response::Short);
+const CMD51: (u8, Response)                 = (51, Response::Short);
 ///
-pub const APP_CMD: (u8, Response)               = (55, Response::Short);
+const APP_CMD: (u8, Response)               = (55, Response::Short);
 
 ///
 /// App Commands
 ///
 
 ///
-pub const ACMD6: (u8, Response)                 = (6, Response::Short);
+const ACMD6: (u8, Response)                 = (6, Response::Short);
 ///
-pub const ACMD13: (u8, Response)                = (13, Response::Short);
+const ACMD13: (u8, Response)                = (13, Response::Short);
 
 impl Cmd {
     const fn from_constant((cmd, resp): (u8, Response), arg: u32) -> Cmd {
         Cmd {cmd, arg, resp}
     }
 
+    /// Put card in idle mode
     pub const fn idle() -> Cmd {
         Cmd::from_constant(IDLE, 0)
     }
 
+    /// Ask all cards to send their Cids
     pub const fn all_send_cid() -> Cmd {
         Cmd::from_constant(ALL_SEND_CID, 0)
     }
 
+    /// Send Rca
     pub const fn send_rel_address() -> Cmd {
         Cmd::from_constant(SEND_REL_ADDR, 0)
     }
@@ -78,8 +84,8 @@ impl Cmd {
         Cmd::from_constant(ACMD13, 0)
     }
 
-    pub const fn sel_desel_card(arg: u32) -> Cmd {
-        Cmd::from_constant(SEL_DESEL_CARD, arg)
+    pub const fn sel_desel_card(rca: u32) -> Cmd {
+        Cmd::from_constant(SEL_DESEL_CARD, rca)
     }
 
     pub const fn hs_send_ext_csd(arg: u32) -> Cmd {
